e0abf963a0d0a224df7437fb0667234fe01c7b11
JoonHo Son <jhson@nagesoft.com>
2015-06-10 15:57:11 +0900

    * LED 3개로 조정.

--- led/node/app.js
+++ led/node/app.js
@@ -1,39 +1,39 @@
 var Gpio = require('onoff').Gpio,
-    led1 = new Gpio(18, 'out');
-    // led2 = new Gpio(46, 'out'),
-    // led3 = new Gpio(65, 'out');
+    led1 = new Gpio(25, 'out'),
+    led2 = new Gpio(24, 'out'),
+    led3 = new Gpio(23, 'out');
 
 var sleep = require('sleep');
 
 process.on('exit', function() {
     console.log('exit program!');
     led1.write(0, function(err){});
-    // led2.write(0, function(err){});
-    // led3.write(0, function(err){});
+    led2.write(0, function(err){});
+    led3.write(0, function(err){});
 });
 
 (function blink() {
-    // led1.read(function(err, value) {
-    //     if (err) {
-    //         throw err;
-    //     }
+    led1.read(function(err, value) {
+        if (err) {
+            throw err;
+        }
 
-    //     if (value) {
-    //         console.log('on');
-    //     } else {
-    //         console.log('off');
-    //     }
+        if (value) {
+            console.log('on');
+        } else {
+            console.log('off');
+        }
 
-    //     led1.write(value ^ 1, function(err) {
-    //         if (err) {
-    //             throw err;
-    //         }
-    //     });
-    // });
+        led1.write(value ^ 1, function(err) {
+            if (err) {
+                throw err;
+            }
+        });
+    });
 
-    // setTimeout(function() {
-    //     blink();
-    // }, 200);
+    setTimeout(function() {
+        blink();
+    }, 200);
     
     while (true) {
         led1.write(1, function(err) {
@@ -42,17 +42,17 @@ process.on('exit', function() {
             }
         });
 
-        // led2.write(0, function(err) {
-        //     if (err) {
-        //         throw err;
-        //     }
-        // });
+        led2.write(0, function(err) {
+            if (err) {
+                throw err;
+            }
+        });
 
-        // led3.write(0, function(err) {
-        //     if (err) {
-        //         throw err;
-        //     }
-        // });
+        led3.write(0, function(err) {
+            if (err) {
+                throw err;
+            }
+        });
 
         sleep.usleep(100000);
 
@@ -62,37 +62,37 @@ process.on('exit', function() {
             }
         });
 
-        // led2.write(1, function(err) {
-        //     if (err) {
-        //         throw err;
-        //     }
-        // });
+        led2.write(1, function(err) {
+            if (err) {
+                throw err;
+            }
+        });
 
-        // led3.write(0, function(err) {
-        //     if (err) {
-        //         throw err;
-        //     }
-        // });
+        led3.write(0, function(err) {
+            if (err) {
+                throw err;
+            }
+        });
 
         sleep.usleep(100000);
 
-        // led1.write(0, function(err) {
-        //     if (err) {
-        //         throw err;
-        //     }
-        // });
+        led1.write(0, function(err) {
+            if (err) {
+                throw err;
+            }
+        });
 
-        // led2.write(0, function(err) {
-        //     if (err) {
-        //         throw err;
-        //     }
-        // });
+        led2.write(0, function(err) {
+            if (err) {
+                throw err;
+            }
+        });
 
-        // led3.write(1, function(err) {
-        //     if (err) {
-        //         throw err;
-        //     }
-        // });
+        led3.write(1, function(err) {
+            if (err) {
+                throw err;
+            }
+        });
 
         sleep.usleep(100000);
     }
--- led/py/test.py
+++ led/py/test.py
@@ -1,41 +1,35 @@
 import RPi.GPIO as GPIO
 import time
 
-LED1 = 18
-# LED2 = "P8_16"
-# LED3 = "P8_18"
-
-# GPIO.setup(LED1, GPIO.OUT)
-# GPIO.setup(LED2, GPIO.OUT)
-# GPIO.setup(LED3, GPIO.OUT)
+LED1 = 25
+LED2 = 24
+LED3 = 23
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED1, GPIO.OUT)
+GPIO.setup(LED2, GPIO.OUT)
+GPIO.setup(LED3, GPIO.OUT)
 
 try:
     while (True):
         GPIO.output(LED1, True)
-        # GPIO.output(LED2, GPIO.LOW)
-        # GPIO.output(LED3, GPIO.LOW)
-        # time.sleep(0.1)
-
-        # GPIO.output(LED1, GPIO.LOW)
-        # GPIO.output(LED2, GPIO.HIGH)
-        # GPIO.output(LED3, GPIO.LOW)
-        # time.sleep(0.1)
-
-        # GPIO.output(LED1, GPIO.LOW)
-        # GPIO.output(LED2, GPIO.LOW)
-        # GPIO.output(LED3, GPIO.HIGH)
-
+        GPIO.output(LED2, False)
+        GPIO.output(LED3, False)
         time.sleep(0.1)
 
         GPIO.output(LED1, False)
+        GPIO.output(LED2, True)
+        GPIO.output(LED3, False)
+        time.sleep(0.1)
+
+        GPIO.output(LED1, False)
+        GPIO.output(LED2, False)
+        GPIO.output(LED3, True)
 
         time.sleep(0.1)
 
 except KeyboardInterrupt:
     GPIO.output(LED1, False)
-    # GPIO.output(LED2, GPIO.LOW)
-    # GPIO.output(LED3, GPIO.LOW)
+    GPIO.output(LED2, False)
+    GPIO.output(LED3, False)
     GPIO.cleanup()
