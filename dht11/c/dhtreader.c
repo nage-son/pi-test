0dccc6a354ffa0e2b07a922bdfc2cff85cbc053b
JoonHo Son <jhson@nagesoft.com>
2015-06-16 10:25:48 +0900

    * Initial commit.

---
+++ dht11/c/dhtreader.c
@@ -0,0 +1,71 @@
+/*  Python module for DHT temperature/humidity sensors
+ *
+ *  Modified by Qingping Hou from DHT reader example, original header:
+ *
+ *  How to access GPIO registers from C-code on the Raspberry-Pi
+ *  Example program
+ *  15-January-2012
+ *  Dom and Gert
+ */
+
+
+/* for usleep */
+#define _BSD_SOURCE
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <assert.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+// #include <bcm2835.h>
+#include <mraa.h>
+#include <unistd.h>
+
+// #include <Python.h>
+
+#define MAXTIMINGS 100
+
+//#define DEBUG
+
+#define DHT11 11
+#define DHT22 22
+#define AM2302 22
+
+int main(int argc, char* argv[]) {
+    int i, pin;
+
+    if (argc != 2) {
+        printf("Input GPIO number...");
+        exit(1);
+    }
+
+    pin = atoi(argv[1]);
+
+    printf("GPIO >>> %d\n", pin);
+
+    mraa_gpio_context gpio;
+
+    gpio = mraa_gpio_init(pin);
+
+    mraa_gpio_dir(gpio, MRAA_GPIO_OUT);
+
+    while (1) {
+        mraa_gpio_write(gpio, 0);
+        printf("Off\n");
+        sleep(1);
+
+        mraa_gpio_write(gpio, 1);
+        printf("On\n");
+        sleep(1);
+    }
+
+    mraa_gpio_close(gpio);
+
+    return 0;
+}
--- dht11/py/test2.py
+++ dht11/py/test2.py
@@ -14,8 +14,22 @@ LED2.dir(mraa.DIR_OUT)
 LED3 = mraa.Gpio(LED_PIN_3)
 LED3.dir(mraa.DIR_OUT)
 
+DHT11_PIN = 3
+
+DHT11 = mraa.Pwm(DHT11_PIN)
+
+DHT11.config_ms(1000, 10)
+DHT11.enable(True)
+
 try:
-    while (True):
+    while True:
+        DHT11.write(0.1)
+        value1, value2 = DHT11.read()
+
+        print "---------------------------"
+        print value1
+        print value2
+
         LED1.write(1)
         LED2.write(0)
         LED3.write(0)
@@ -35,3 +49,4 @@ except KeyboardInterrupt:
     LED1.write(0)
     LED2.write(0)
     LED3.write(0)
+    print "DHT11 end"
